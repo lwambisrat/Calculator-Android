2a94d90004e3e641349b05150d1c63a4c417c014
lwam <lwambsrat@gmail.com>
2025-06-23 18:14:31 +0300

    doc:update calculation function

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -47,42 +47,93 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         btnAdd.setOnClickListener{
-            var num1=etNum1.text.toString()
-            var num2=etNum2.text.toString()
-            var num3=etNum3.text.toString()
-            var num4=etNum4.text.toString()
-            var num5=etNum5.text.toString()
-            var sum=num1.toInt()+num2.toInt()+num3.toInt()+num4.toInt()+num5.toInt()
-            tvResult.text=sum.toString()
 
+
+//            var num1=etNum1.text.toString()
+//            var num2=etNum2.text.toString()
+//            var num3=etNum3.text.toString()
+//            var num4=etNum4.text.toString()
+//            var num5=etNum5.text.toString()
+//            var sum=num1.toInt()+num2.toInt()+num3.toInt()+num4.toInt()+num5.toInt()
+
+        obtainValues("+")
         }
         btnSubtract.setOnClickListener(){
-            var num1=etNum1.text.toString()
-            var num2=etNum2.text.toString()
-            var num3=etNum3.text.toString()
-            var num4=etNum4.text.toString()
-            var num5=etNum5.text.toString()
-            var subtract=num1.toInt()-num2.toInt()-num3.toInt()-num4.toInt()-num5.toInt()
-            tvResult.text=subtract.toString()
+//            var num1=etNum1.text.toString()
+//            var num2=etNum2.text.toString()
+//            var num3=etNum3.text.toString()
+//            var num4=etNum4.text.toString()
+//            var num5=etNum5.text.toString()
+//            var subtract=num1.toInt()-num2.toInt()-num3.toInt()-num4.toInt()-num5.toInt()
+            obtainValues("-")
+
         }
         btnDiv.setOnClickListener(){
-            var num1=etNum1.text.toString()
-            var num2=etNum2.text.toString()
-            var num3=etNum3.text.toString()
-            var num4=etNum4.text.toString()
-            var num5=etNum5.text.toString()
-            var division=num1.toInt()/num2.toInt()/num3.toInt()/num4.toInt()/num5.toInt()
-            tvResult.text=division.toString()
+//            var num1=etNum1.text.toString()
+//            var num2=etNum2.text.toString()
+//            var num3=etNum3.text.toString()
+//            var num4=etNum4.text.toString()
+//            var num5=etNum5.text.toString()
+//            var division=num1.toInt()/num2.toInt()/num3.toInt()/num4.toInt()/num5.toInt()
+//            tvResult.text=division.toString()
+            obtainValues("/")
         }
         btnMulti.setOnClickListener(){
+//            var num1=etNum1.text.toString()
+//            var num2=etNum2.text.toString()
+//            var num3=etNum3.text.toString()
+//            var num4=etNum4.text.toString()
+//            var num5=etNum5.text.toString()
+//            var multi=num1.toInt()*num2.toInt()*num3.toInt()*num4.toInt()*num5.toInt()
+//            tvResult.text=multi.toString()
+           obtainValues("*")
+        }
+    }
+        fun obtainValues(symbol:String){
             var num1=etNum1.text.toString()
             var num2=etNum2.text.toString()
             var num3=etNum3.text.toString()
             var num4=etNum4.text.toString()
-            var num5=etNum5.text.toString()
-            var multi=num1.toInt()*num2.toInt()*num3.toInt()*num4.toInt()*num5.toInt()
-            tvResult.text=multi.toString()
-        }
+         var  num5=etNum5.text.toString()
+            if(num1.isBlank()){
+                etNum1.error="Num 1 is missed"
+                return
+            }
+            if(num2.isBlank()){
+                etNum2.error="Num 2 is missed"
+                return
+            }
+            if(num3.isBlank()){
+                etNum3.error="Num 3 is missed"
+                return
+            }
+            if(num4.isBlank()){
+                etNum4.error="Num 4 is missed"
+                return
+            }
+            if(num5.isBlank()){
+                etNum5.error="Num 5 is missed"
+                return
+            }
 
+//               return when(symbol){
+//                "+"->num1.toDouble()+num2.toDouble()+num3.toDouble()+num4.toDouble()+num5.toInt()
+//                "-"->num1.toDouble()-num2.toDouble()-num3.toDouble()-num4.toInt()-num5.toInt()
+//                "*"->num1.toDouble()*num2.toInt()*num3.toInt()*num4.toDouble()*num5.toInt()
+//                "/"->num1.toDouble()/num2.toDouble()/num3.toDouble()/num4.toDouble()/num5.toInt()
+//                else -> 0
+//            }
+             performCalculation(symbol,num1,num2,num3,num4,num5)
+    }
+    fun performCalculation(symbol:String,num1:String,num2: String,num3: String,num4: String,num5: String){
+
+        var result= when(symbol){
+            "+"->num1.toDouble()+num2.toDouble()+num3.toDouble()+num4.toDouble()+num5.toInt()
+            "-"->num1.toDouble()-num2.toDouble()-num3.toDouble()-num4.toInt()-num5.toInt()
+            "*"->num1.toDouble()*num2.toInt()*num3.toInt()*num4.toDouble()*num5.toInt()
+            "/"->num1.toDouble()/num2.toDouble()/num3.toDouble()/num4.toDouble()/num5.toInt()
+            else -> 0.0
+        }
+        tvResult.text=result.toString()
     }
 }
